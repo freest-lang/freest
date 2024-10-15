ee0468fcfb757144490a22cb71a047c248622cee
Tomás Carreira <carreira_1012@hotmail.com>
2024-10-15 11:29:19 +0100

    Add some builtin functions. Change the context to use only string instead of a B.Variable

--- freest/src/Interpreter/Interpreter.hs
+++ freest/src/Interpreter/Interpreter.hs
@@ -23,17 +23,59 @@ data Value = VInt Int
             | VFun [([E.Pat], E.RHS)]
             -- do closures capture only the local ctx or also the global?
             | VClosure [E.Pat] E.Exp Context
-            deriving Show
+            | VBuiltin (Value -> Value)
+
+instance Show Value where
+  show VUnit = "()"
+  show (VInt n) = show n
+  show (VFloat n) = show n
+  show (VChar c) = show c
+  show (VString str) = show str
+  show (VCons str) = show str
+  show (VFun _) = "<fun>"
+  show (VClosure _ _ _) = "closure>"
+  show (VBuiltin _) = "<builtin>"
+
+-- There must be a better way to do this
+builtins :: [(String , Value)]
+builtins = [
+  ("(+)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (x + y)))),
+  ("(-)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (x - y)))),
+  ("(*)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (x * y)))),
+  ("(/)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (div x y)))),
+  ("mod", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (mod x y)))),
+  ("negate", VBuiltin (\(VInt x) -> VInt (-x))),
+  
+  -- TODO: Eww, refactor this
+  ("(&&)", VBuiltin (\x -> VBuiltin (\y -> hsToFstBool ((fstToHsBool x) && (fstToHsBool y))))),
+  ("(||)", VBuiltin (\x -> VBuiltin (\y -> hsToFstBool ((fstToHsBool x) || (fstToHsBool y))))),
+
+  ("(==)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> hsToFstBool (x == y)))),
+  ("(/=)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> hsToFstBool (x /= y)))),
+  ("(>)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> hsToFstBool (x > y)))),
+  ("(>=)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> hsToFstBool (x >= y)))),
+  ("(<)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> hsToFstBool (x < y)))),
+  ("(<=)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> hsToFstBool (x <= y))))]
+
+-- haskell bool to freest bool
+hsToFstBool :: Bool -> Value
+hsToFstBool True = VCons "True"
+hsToFstBool False = VCons "False"
+
+-- freeST bool to haskell bool 
+fstToHsBool :: Value -> Bool
+fstToHsBool (VCons "True") = True
+fstToHsBool (VCons "False") = False
 
 -- Using a simple context for now for simplicity
-type Context = [(B.Variable, Value)]
+type Context = [(String, Value)]
 
 interpret :: M.Module -> Either [IOE.Error] Value
 interpret m = case getMainFunction (trace (show m) m) of
   -- Assuming that the RHS of main is always in the form main = <exp>
   -- necessary to initialize the context with information from the module
   -- other modules, prelude, etc
-  Just (E.ValDecl _ (E.UnguardedRHS mainExp _)) -> Right $ eval (initContext m, []) mainExp
+  Just (E.ValDecl _ (E.UnguardedRHS mainExp _)) -> Right $ eval (initContext m ++ builtins, []) mainExp
   -- Return unit when main function is not present
   Nothing -> Right VUnit
 
@@ -48,8 +90,8 @@ initContext :: M.Module -> Context
 initContext m =
   -- is VarPat the only valid pattern in a valDecl??
   -- the same for the rhs UnguardedRHS
-  map (\def -> case def of E.ValDecl (E.VarPat _ var) (E.UnguardedRHS exp _) -> (var, eval ([], []) exp)
-                           E.FnDecl var fun -> (var, VFun (map (\(levels, rhs) -> ((map (\(B.ExpLevel pat) -> pat) (filterTypesFromLevels levels)), rhs)) fun))
+  map (\def -> case def of E.ValDecl (E.VarPat _ var) (E.UnguardedRHS exp _) -> (B.external var, eval (builtins, []) exp)
+                           E.FnDecl var fun -> (B.external var, VFun (map (\(levels, rhs) -> ((map (\(B.ExpLevel pat) -> pat) (filterTypesFromLevels levels)), rhs)) fun))
   -- do not add main to the context
   ) (filter (\def -> case def of E.ValDecl (E.VarPat _ var) _ -> B.external var /= "main" 
                                  E.SigDecl _ _ -> False
@@ -76,7 +118,7 @@ eval ctx (E.App _ exp levels) =
 eval (_, local) (E.Abs _ levels _ exp) = VClosure (map (\(B.ExpLevel (pat, _)) -> pat) (filterTypesFromLevels levels)) exp local
 eval _ (E.Let _ letDecl exp) = trace ("Let -> letDecl: " ++ show letDecl ++ " exp: " ++ show exp) undefined 
 eval _ (E.Case _ exp pats) = trace ("Case -> exp: " ++ show exp ++ " pats: " ++ show pats) undefined
-eval ctx (E.If _ ifExp thenExp elseExp) = if isTrue (eval ctx ifExp) then eval ctx thenExp else eval ctx elseExp
+eval ctx (E.If _ ifExp thenExp elseExp) = if fstToHsBool (eval ctx ifExp) then eval ctx thenExp else eval ctx elseExp
 eval _ (E.Channel _ type') = trace ("Channel -> type: " ++ show type') undefined
 eval _ (E.Select _ iden) = trace ("Select -> iden: " ++ show iden) undefined
 
@@ -85,16 +127,11 @@ eval _ (E.Select _ iden) = trace ("Select -> iden: " ++ show iden) undefined
 -- Eww ungly, find better way of doing this (>>= but the opposite?)
 -- TODO: Perguntar ao Gil como é que é utilizado o campo internal das Variable para saber se as posso usar desta maneira
 getVar :: (Context, Context) -> B.Variable -> Value
-getVar (global, local) var = case find (\(var2, val) -> B.external var == B.external var2) local of
+getVar (global, local) var = case find (\(var2, val) -> B.external var == var2) local of
   Just (var, val) -> val
-  Nothing -> case find (\(var2, val) -> B.external var == B.external var2) global of
+  Nothing -> case find (\(var2, val) -> B.external var == var2) global of
     Just (var, val) -> val
     Nothing -> error ("Variable `" ++ show var ++ "`not found in the context. This should not happen. This is a bug in the compiler")
-
--- check if value is true (inside FreeST)
-isTrue :: Value -> Bool
-isTrue (VCons "True") = True
-isTrue (VCons "False") = False
 
 -- removes the type arguments (i.e. @Int) from arguments
 filterTypesFromLevels :: [B.Level a b] -> [B.Level a b]
@@ -125,16 +162,20 @@ consumeAllArgs (global, local) (VFun patExps) args = case chooseRhs patExps args
       E.UnguardedRHS exp Nothing -> VClosure (drop (length args) pats) exp matched
   -- TODO: use freeST error handling to tell the user that that pattern mathcing was not exhautive
   Nothing -> undefined
+-- Is there builtins that take no arguments?
+consumeAllArgs ctx (VBuiltin builtin) [] = builtin VUnit
+consumeAllArgs ctx (VBuiltin builtin) [arg] = builtin arg
+consumeAllArgs ctx (VBuiltin builtin) (arg:args) = consumeAllArgs ctx (builtin arg) args
  
 -- TODO: refactor to use map and filter?
-doPatternMatching :: [E.Pat] -> [Value] -> [Maybe (B.Variable, Value)]
+doPatternMatching :: [E.Pat] -> [Value] -> [Maybe (String, Value)]
 doPatternMatching [] [] = []
 doPatternMatching pats [] = []
 doPatternMatching [] args = []
 -- TODO: finish implementing the rest of the patterns (ConsPat is partially implemented)
 doPatternMatching (pat:pats) (arg:args) = case pat of
   E.WildPat _ _ -> doPatternMatching pats args
-  E.VarPat _ var -> Just (var, arg) : doPatternMatching pats args
+  E.VarPat _ var -> Just (B.external var, arg) : doPatternMatching pats args
   E.ConsPat _ (B.Identifier _ str) pats -> if (\(VCons str) -> str) arg == str then doPatternMatching pats args else Nothing : doPatternMatching pats args
   E.TuplePat _ _ -> undefined
   E.IntPat _ n -> if (\(VInt n) -> n) arg == n then doPatternMatching pats args else Nothing : doPatternMatching pats args 
@@ -143,7 +184,7 @@ doPatternMatching (pat:pats) (arg:args) = case pat of
   E.StringPat _ str -> if (\(VString str) -> str) arg == str then doPatternMatching pats args else Nothing : doPatternMatching pats args
   E.AsPat _ var pat -> undefined
 
-chooseRhs :: [([E.Pat], E.RHS)] -> [Value] -> Maybe (E.RHS, [(B.Variable, Value)], [E.Pat])
+chooseRhs :: [([E.Pat], E.RHS)] -> [Value] -> Maybe (E.RHS, [(String, Value)], [E.Pat])
 chooseRhs [] _ = Nothing
 chooseRhs ((pats, rhs):rest) args = case sequence $ doPatternMatching pats args of
   Just matching -> Just (rhs, matching, pats)
