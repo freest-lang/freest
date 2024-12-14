feecc1cff3e06459242c2e9698e4a4330d7ddf42
Vasco T. Vasconcelos <vmvasconcelos@ciencias.ulisboa.pt>
2024-12-14 00:06:57 +0000

    tuning

--- freest/src/TypeEquivalence/TypeEquivalence.hs
+++ freest/src/TypeEquivalence/TypeEquivalence.hs
@@ -18,10 +18,6 @@ import           SimpleGrammar.FromType        ( fromType )
 -- import qualified Bisimulation.Bisimulation     ( bisimilar )
 
 equivalent :: TypeDeclMap -> T.Type -> T.Type -> Bool
-equivalent td t u = t == u || bisimilar td t u
-
-bisimilar :: TypeDeclMap -> T.Type -> T.Type -> Bool
-bisimilar td t u = let _ = fromType td [t, u] in True
--- bisimilar td t u = bisimilar $ fromType td [t, u]
+equivalent td t u = t == u || True -- bisimilar $ fromType td [t, u]
 
 -- TODO: Move this code to module Bisimulation.Bisimulation
