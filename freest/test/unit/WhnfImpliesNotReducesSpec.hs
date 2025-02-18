b5aa3bb79119b69d21d9cb6b4e9d32800acc7ddd
Vasco T. Vasconcelos <vmvasconcelos@ciencias.ulisboa.pt>
2025-02-18 21:41:44 +0000

    WhnfImpliesNotReduces test

--- freest/test/unit/WhnfImpliesNotReducesSpec.hs
+++ freest/test/unit/WhnfImpliesNotReducesSpec.hs
@@ -1,4 +1,4 @@
-module ReduceImpliesNotWhnfSpec (spec) where
+module WhnfImpliesNotReducesSpec (spec) where
 
 import qualified Syntax.Module                 as M
 import qualified Syntax.Type                   as T
@@ -20,14 +20,15 @@ main = hspec spec
 spec :: Spec
 spec = mkKindingSpec
   ["test/unit/WellFormedTypes.test"] 
-  "If T reduces then T is not a whnf" 
-  \(t, _, m) -> reducesImpliesNotWhnf (buildDataDecls m) t >>= (`shouldBe` True)
+  "If T is a whnf then T does not reduce" 
+  \(t, _, m) -> whnfImpliesNotReduces (buildDataDecls m) t >>= (`shouldBe` True)
 
-reducesImpliesNotWhnf :: TypeDeclMap -> T.Type -> IO Bool
-reducesImpliesNotWhnf m t =
-  catch
-    (let !_ = reduce m t in pure (not (isWhnf t))) -- t reduces and is not in whnf
-    (\(x::ErrorCall) -> pure True) -- t does not reduce
+whnfImpliesNotReduces :: TypeDeclMap -> T.Type -> IO Bool
+whnfImpliesNotReduces m t
+  | isWhnf t = catch
+      (let !_ = reduce m t in pure False)
+      (\(x::ErrorCall) -> pure True)
+  | otherwise = pure True
 
 -- Warning: code also in from Validation.Base
 buildDataDecls :: M.Module -> TypeDeclMap
