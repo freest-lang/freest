50edec7f130d218130a06c8500fc5a5831a10d94
Pedro Ângelo <pjfangelo@hotmail.com>
2025-11-20 23:29:39 +0000

    minor cosmetic changes to Interpreter.hs

--- freest/src/Interpreter/Interpreter.hs
+++ freest/src/Interpreter/Interpreter.hs
@@ -1,11 +1,27 @@
-module Interpreter.Interpreter where
+{- |
+Module      :  Interpreter.Interpreter
+Copyright   :  © The FreeST Team
+Maintainer  :  freest-lang@listas.ciencias.ulisboa.pt
 
-import Data.List ( find )
+This module contains utilities for parsing, namely for generating fresh variables
+and constructing types and expressions more succinctly.
+-}
+
+{-
+TODO:
+- Change the environment from an association list to a map
+-}
+module Interpreter.Interpreter
+  (
+    interpret
+  ) where
+
+import Data.List (find)
 import Data.Char (chr, ord)
-import Data.Functor ( ($>), (<&>), void )
-import System.IO ( Handle, putStr, hPutStr, getChar, getLine, getContents, stderr, openFile, IOMode(..), hGetChar, hGetLine, hIsEOF, hClose )
-import Control.Concurrent ( forkIO )
-import Control.Concurrent.Chan as C
+import Data.Functor (($>), (<&>), void)
+import System.IO (Handle, putStr, hPutStr, getChar, getLine, getContents, stderr, openFile, IOMode(..), hGetChar, hGetLine, hIsEOF, hClose)
+import Control.Concurrent (forkIO)
+import qualified Control.Concurrent.Chan as C (Chan, newChan, readChan, writeChan)
 import GHC.Float
 -- for debuging don't forget to remove
 import Debug.Trace
@@ -19,20 +35,20 @@ import Syntax.Expression ( LetDecl )
 type ChannelEnd = (C.Chan Value, C.Chan Value)
 
 data Value = VInt Int
-            | VFloat Double
-            | VUnit 
-            | VChar Char
-            | VString String
-            | VCons String [Value]
-            -- | VTuple [Value]
-            | VFun [([E.Pat], E.RHS)]
-            | VClosure [E.Pat] E.Exp Context
-            | VBuiltin (Value -> Value)
-            | VIO (IO Value)
-            | VHandle Handle
-            | VLabel String
-            | VFork
-            | VChan ChannelEnd
+           | VFloat Double
+           | VUnit
+           | VChar Char
+           | VString String
+           | VCons String [Value]
+           -- | VTuple [Value]
+           | VFun [([E.Pat], E.RHS)]
+           | VClosure [E.Pat] E.Exp Env
+           | VBuiltin (Value -> Value)
+           | VIO (IO Value)
+           | VHandle Handle
+           | VLabel String
+           | VFork
+           | VChan ChannelEnd
 
 instance Show Value where
   show VUnit = "()"
@@ -43,7 +59,7 @@ instance Show Value where
   show (VCons str vals) = str ++ " " ++ unwords (map show vals)
   -- show (VTuple tups) = "(" ++ showTups tups ++ ")"
   show (VFun _) = "<fun>"
-  show (VClosure _ _ _) = "closure>"
+  show (VClosure _ _ _) = "<closure>"
   show (VBuiltin _) = "<builtin>"
   show (VIO io) = "<IO>"
   show (VHandle _) = "<handle>"
@@ -54,8 +70,8 @@ showTups :: [Value] -> String
 showTups [val] = show val
 showTups (val:vals) = show val ++ ", " ++ showTups vals
 
-tupToList :: (a,a) -> [a]
-tupToList (x, y) = [x, y]
+{- tupToList :: (a,a) -> [a]
+tupToList (x, y) = [x, y] -}
 
 chan :: IO (ChannelEnd, ChannelEnd)
 chan = do
@@ -87,7 +103,7 @@ close (VChan c) = do
   C.writeChan (snd c) VUnit
   return VUnit
 
-builtins :: [(String , Value)]
+builtins :: [(String, Value)]
 builtins = [
   ("receive", VBuiltin (\(VChan c) -> VIO $ receive c >>= \(val, c) -> return $ VCons "(,)" [val, VChan c])),
   ("send", VBuiltin (\val -> VBuiltin (\(VChan c) -> VIO $ VChan <$> send val c))),
@@ -203,53 +219,75 @@ fstToHsBool (VCons "False" []) = False
 
 -- Using a simple context for now for simplicity
 -- TODO: change to a hashmap
-type Context = [(String, Value)]
+type Env = [(String, Value)]
+type GlobalEnv = Env
+type LocalEnv = Env
 
 interpret :: M.Module -> IO Value
 interpret m = case getMainFunction m of
   -- Assuming that the RHS of main is always in the form main = <exp>
   -- necessary to initialize the context with information from the module
   -- other modules, prelude, etc
-  Just (E.ValDecl _ (E.UnguardedRHS mainExp _)) -> do initial_ctx <- initContext m
-                                                      eval (initial_ctx ++ builtins, []) mainExp
+  Just (E.ValDef pat rhs) -> case rhs of
+    E.UnguardedRHS mainExp whereDefs -> do
+      initial_ctx <- initEnv m
+      putStrLn $ "Expression is: " ++ show mainExp
+      eval (initial_ctx ++ builtins, []) mainExp
+    _ -> do
+      return VUnit
+  Just _ -> do return VUnit
   -- Return unit when main function is not present
   Nothing -> do return VUnit
 
-getMainFunction :: M.Module -> Maybe LetDecl 
+{-
+  Just (E.ValDef _ (E.UnguardedRHS mainExp _)) -> do
+    initial_ctx <- initEnv m
+    eval (initial_ctx ++ builtins, []) mainExp
+  Nothing -> do return VUnit -}
+
+getMainFunction :: M.Module -> Maybe LetDecl
 getMainFunction m = find foo (M.definitions m)
   -- main should be a ValDecl VarPat because it is the form main = <body>
-  where foo funDef = case funDef of E.ValDecl (E.VarPat _ var) _ -> B.external var == "main"  
-                                    _ -> False
+  where
+    foo funDef = case funDef of
+      E.ValDef (E.VarPat _ var) _ -> B.external var == "main"
+      _ -> False
 
 -- For now add only definitions to the context.  
-initContext :: M.Module -> IO Context
-initContext m =
+initEnv :: M.Module -> IO Env
+initEnv m =
   -- is VarPat the only valid pattern in a valDecl??
   -- the same for the rhs UnguardedRHS
-  sequence $ map (\def -> case def of E.ValDecl (E.VarPat _ var) (E.UnguardedRHS exp _) -> do initial_ctx <- eval (builtins, []) exp
-                                                                                              return (B.external var,  initial_ctx)
-                                      E.FnDecl var fun -> do return $ (B.external var, VFun (map (\(levels, rhs) -> ((map (\(B.ExpLevel pat) -> pat) (filterTypesFromLevels levels)), rhs)) fun))
+  sequence $ map (\def -> case def of
+    E.ValDef (E.VarPat _ var) (E.UnguardedRHS exp _) -> do
+      initial_ctx <- eval (builtins, []) exp
+      return (B.external var,  initial_ctx)
+    E.FnDef var fun -> do return $ (B.external var, VFun (map (\(levels, rhs) -> ((map (\(B.ExpLevel pat) -> pat) (filterTypesFromLevels levels)), rhs)) fun))
   -- do not add main to the context
-  ) (filter (\def -> case def of E.ValDecl (E.VarPat _ var) _ -> B.external var /= "main" 
-                                 E.SigDecl _ _ -> False
+  ) (filter (\def -> case def of E.ValDef (E.VarPat _ var) _ -> B.external var /= "main"
+                                 E.TypeSig _ _ -> False
                                  _ -> True)
     (M.definitions m))
 
--- TODO: eval can failed (pattern matching so the return value should be Either [IOE.Error] Value)
--- Global and local context
-eval :: (Context, Context) -> E.Exp -> IO Value
+-- Evaluates expressions, written with Syntax.Expression.Exp.
+{-
+  TODO:
+  1. Eval can fail due to non-existent patterns during pattern marching.
+     Hence return type should Either [IOE.Error] Value.
+
+  Questions:
+  1. What can variables be?
+     Can then be reserved keywords, or that's reserved for cons?
+-}
+eval :: (GlobalEnv, LocalEnv) -> E.Exp -> IO Value
 eval _ (E.Int _ n) = return $ VInt n 
 eval _ (E.Float _ n) = return $ VFloat n
 eval _ (E.Char _ c) = return $ VChar c
--- eval _ (E.String _ str) = return $ VString str
--- [Exp] -> [Value]
--- eval ctx (E.Tuple _ tup) = do
---   vals <- sequence $ map (eval ctx) tup
---   return $ VTuple vals 
-eval _ (E.Cons _ (B.Identifier _ str)) = return $ VCons str []
-eval ctx (E.Var _ var) = case getVar ctx var of VIO io -> io
-                                                val -> return val
-eval (global, local) (E.App _ exp levels) = do
+eval _ (E.DCons _ (B.Identifier _ str)) = return $ VCons str []
+eval (global, local) (E.Var _ var) = case envLookup (global, local) var of
+  VIO io -> io
+  val -> return val
+{- eval (global, local) (E.App _ exp levels) = do
   args <- sequence $ map (\(B.ExpLevel exp) -> eval (global, local) exp) (filterTypesFromLevels levels)
   left <- (eval (global, local) exp)
   case left of
@@ -257,54 +295,76 @@ eval (global, local) (E.App _ exp levels) = do
     _ -> do res <- consumeAllArgs (global, local) left args
             case res of
               VIO io -> io
-              _ -> return res
-eval (_, local) (E.Abs _ levels _ exp) = return $ VClosure (map (\(B.ExpLevel (pat, _)) -> pat) (filterTypesFromLevels levels)) exp local
-eval (global, local) (E.Let _ letDecls exp) = do
+              _ -> return res -}
+eval (_, local) (E.Abs _ levels _ exp) =
+  -- remove type variable parameters, as these are not useful during reduction
+  let expParams = filter (\case B.ExpLevel a -> True; B.TypeLevel b -> False) levels
+  in return $ VClosure (map (\(B.ExpLevel (pat, _)) -> pat) expParams) exp local
+{- eval (global, local) (E.Let _ letDecls exp) = do
   letDeclsCtx <- resolveLetDecls global (filterTypesFromLetDecls letDecls)
-  eval (global, letDeclsCtx ++ local) exp
-eval (global, local) (E.Case _ exp pats) = do
+  eval (global, letDeclsCtx ++ local) exp -}
+{- eval (global, local) (E.Case _ exp pats) = do
   val <- (eval (global, local) exp)
   labels <- mapM receiveLabel $ getInternalChoiceChannels [fst $ head pats] [val]
   case chooseCase pats val labels of
     Just (exp, matched) -> eval (global, matched ++ local) exp 
   -- TODO: use freeST error handling to tell the user that that pattern mathcing was not exhautive
-    Nothing -> undefined
+    Nothing -> undefined -}
 eval ctx (E.If _ ifExp thenExp elseExp) = do
-  ifVal <- (eval ctx ifExp)
+  ifVal <- eval ctx ifExp
   thenVal <- eval ctx thenExp
   elseVal <- eval ctx elseExp
   if fstToHsBool ifVal then return thenVal else return elseVal
 eval _ (E.Channel _ _) = do
   (chanL, chanR) <- chan
-  return $ VCons ("(,)") [VChan chanL, VChan chanR]
-eval ctx (E.Select _ (B.Identifier _ iden) chan) = do
+  return $ VCons "(,)" [VChan chanL, VChan chanR]
+{- eval ctx (E.Select _ (B.Identifier _ iden) chan) = do
   VChan chan2 <- eval ctx chan
   chan3 <- send (VLabel iden) chan2
-  return $ VChan chan3
+  return $ VChan chan3 -}
 
-getVar :: (Context, Context) -> B.Variable -> Value
-getVar _ (B.Variable {B.varSpan=_, B.internal=_, B.external="fork"}) = VFork
-getVar (global, local) var = case find (\(var2, val) -> B.external var == var2) local of
-  Just (var, val) -> val
-  Nothing -> case find (\(var2, val) -> B.external var == var2) global of
-    Just (var, val) -> val
-    Nothing -> error ("Variable `" ++ show var ++ "`not found in the context. This should not happen. This is a bug in the compiler")
+-- catch all, used for debugging
+eval _ exp = do
+  putStrLn $ "Evaluating " ++ show exp
+  return VUnit
+-- eval _ (E.String _ str) = return $ VString str
+-- [Exp] -> [Value]
+-- eval ctx (E.Tuple _ tup) = do
+--   vals <- sequence $ map (eval ctx) tup
+--   return $ VTuple vals
 
+-- lookup a variable in both local and global context, in that order
+envLookup :: (GlobalEnv, LocalEnv) -> B.Variable -> Value
+envLookup _ (B.Variable{B.varSpan=_, B.internal=_, B.external="fork"}) = VFork
+envLookup (global, local) var =
+  -- search in local context first
+  case ctxLookup local var of
+    Just (_, val) -> val
+    -- search in global context after
+    Nothing -> case ctxLookup global var of
+      Just (_, val) -> val
+      Nothing -> error ("Variable `" ++ show var ++ "` not found in the context." ++
+                       " This should not happen. This is a bug in the compiler")
+  where
+    ctxLookup :: Env -> B.Variable -> Maybe (String, Value)
+    ctxLookup ctx var = find (\(variable, value) -> B.external var == variable) ctx
+
+-- TODO REMOVE
 -- removes the type arguments (i.e. @Int) from arguments
 filterTypesFromLevels :: [B.Level a b] -> [B.Level a b]
-filterTypesFromLevels = filter (\level -> case level of B.ExpLevel a -> True
-                                                        B.TypeLevel b -> False) 
+filterTypesFromLevels = filter (\case B.ExpLevel a -> True; B.TypeLevel b -> False)
 
 filterTypesFromLetDecls :: [LetDecl] -> [LetDecl]
-filterTypesFromLetDecls = filter (\letDecl -> case letDecl of E.ValDecl _ _ -> True
-                                                              E.FnDecl _ _ -> True
-                                                              E.SigDecl _ _ -> False)
+filterTypesFromLetDecls = filter (\letDecl -> case letDecl of
+  E.ValDef _ _ -> True
+  E.FnDef _ _ -> True
+  E.TypeSig _ _ -> False)
 
 -- Here is where the function application is done.
 -- Because of how the parser parses function applications (f a b c d => f [a, b, c, d] even if f only takes one arg)
 -- is necessary to repeat the evaluation until [arg] is empty.
 -- TODO: context is a mess must check if it is correct, don't like that there is a lot of repetition
-consumeAllArgs :: (Context, Context) -> Value -> [Value] -> IO Value
+consumeAllArgs :: (Env, Env) -> Value -> [Value] -> IO Value
 consumeAllArgs (global, local) (VClosure pats exp local_ctx) args = case sequence (doPatternMatching pats args []) of
   Just patternMatching ->
     if length pats == length args then eval (global, patternMatching ++ local_ctx ++ local) exp
@@ -337,9 +397,9 @@ consumeAllArgs ctx (VBuiltin builtin) (arg:args) = consumeAllArgs ctx (builtin a
 consumeAllArgs ctx (VCons str vals) args = return $ VCons str (vals++args) 
 
 -- TODO: think of a better name for this function
-resolveLetDecls :: Context -> [LetDecl] -> IO [(String, Value)]
+resolveLetDecls :: Env -> [LetDecl] -> IO [(String, Value)]
 resolveLetDecls _ [] = return []
-resolveLetDecls global ((E.ValDecl pat rhs):letDecls) = do
+resolveLetDecls global ((E.ValDef pat rhs):letDecls) = do
   (exp, whereDecls) <- case rhs of E.UnguardedRHS exp whereDecls -> return (exp, whereDecls)
                                    E.GuardedRHS predExps whereDecls -> do guardsCtx <- chooseGuard (global, []) predExps
                                                                           return (guardsCtx, whereDecls)
@@ -352,11 +412,11 @@ resolveLetDecls global ((E.ValDecl pat rhs):letDecls) = do
   -- TODO: use freeST error handling to tell the user that that pattern mathcing was not exhautive
     Nothing -> undefined
 
-resolveLetDecls global ((E.FnDecl var levelRhss):letDecls) = do
+resolveLetDecls global ((E.FnDef var levelRhss):letDecls) = do
   letDeclsCtx <- resolveLetDecls global letDecls 
   return $ (B.external var, VFun (map (\(levels, rhs) -> (map (\(B.ExpLevel pat) -> pat) (filterTypesFromLevels levels), rhs)) levelRhss)) : letDeclsCtx
 
--- TODO: create a resolveWhereDecls for more readable code (Context -> Maybe [LetDecl] -> [(String, Value)])
+-- TODO: create a resolveWhereDecls for more readable code (Env -> Maybe [LetDecl] -> [(String, Value)])
 
 -- TODO: refactor to use map and filter?
 -- do i need to do Nothing : doPatternMatching pats args or can i just return Nothing
@@ -367,7 +427,7 @@ doPatternMatching [] args _ = []
 doPatternMatching (pat:pats) (arg:args) labels = case pat of
   E.WildPat _ _ -> doPatternMatching pats args labels
   E.VarPat _ var -> Just (B.external var, arg) : doPatternMatching pats args labels
-  E.ConsPat _ (B.Identifier _ patIden) consPats -> case arg of
+  E.DConsPat _ (B.Identifier _ patIden) consPats -> case arg of
     VCons iden consArgs -> if iden == patIden then doPatternMatching consPats consArgs labels ++ doPatternMatching pats args labels else Nothing : doPatternMatching pats args labels
     VChan chan -> if patIden == head labels then Just (B.external $ (\[E.VarPat _ var] -> var) consPats, VChan chan) : doPatternMatching pats args (tail labels) else Nothing : doPatternMatching pats args (tail labels)
   -- E.TuplePat _ tupPats -> doPatternMatching tupPats ((\(VTuple tupVals) -> tupVals) arg) ++ doPatternMatching pats args
@@ -383,7 +443,7 @@ getInternalChoiceChannels [] [] = []
 getInternalChoiceChannels pats [] = []
 getInternalChoiceChannels [] args = []
 getInternalChoiceChannels (pat:pats) (arg:args) = case pat of
-  E.ConsPat _ (B.Identifier _ patIden) patCons -> case arg of
+  E.DConsPat _ (B.Identifier _ patIden) patCons -> case arg of
     VCons _ consArgs -> getInternalChoiceChannels patCons consArgs ++ getInternalChoiceChannels pats args
     VChan chan -> chan : getInternalChoiceChannels pats args
   _  -> getInternalChoiceChannels pats args
@@ -401,7 +461,7 @@ chooseCase ((pat, exp):patsExps) val labels = case doPatternMatching [pat] [val]
   [Just (var, val)] -> Just (exp, [(var, val)])
   [Nothing] -> chooseCase patsExps val labels
 
-chooseGuard :: (Context, Context) -> [(E.Exp, E.Exp)] -> IO E.Exp
+chooseGuard :: (Env, Env) -> [(E.Exp, E.Exp)] -> IO E.Exp
 -- TODO: error for no exaustive guards
 chooseGuard _ [] = undefined 
 chooseGuard ctx ((pred, exp):predExps) = do
