59fd6a367493abdd8721e90b34daf48420762006
Bernardo Almeida <bernardo.pinto.almeida@gmail.com>
2026-01-29 01:24:05 +0000

    tuning kinded types

--- freest/src/Syntax/Type/Kinded.hs
+++ freest/src/Syntax/Type/Kinded.hs
@@ -52,9 +52,10 @@ module Syntax.Type.Parsed (
   )
 where
 
-import Syntax.Type.Internal qualified as T
 import Syntax.Base
 import Syntax.Kind qualified as K
+import Syntax.Names
+import Syntax.Type.Internal qualified as T
 
 type KindedType = T.Type Kinded
 
@@ -130,68 +131,92 @@ pattern Var s k a <- T.Var s k a
 pattern Abs :: Span -> [(Variable, K.Kind)] -> KindedType -> KindedType
 pattern Abs s aks t <- T.Abs s _ aks t
   where Abs s aks t = T.Abs s k aks t
-          where k = foldr (K.Arrow s . snd ) (K.lt s) aks
+          where k = foldr (K.Arrow s . snd ) (kindOf t) aks
 
--- pattern App :: Span -> KindedType -> [KindedType] -> KindedType
--- pattern App s t ts <- T.App s _ t ts
---   where App s t ts = T.App s void t ts
+pattern App :: Span -> KindedType -> [KindedType] -> KindedType
+pattern App s t ts <- T.App s _ t ts
+  where App s t ts = T.App s k t ts
+          where k = foldr (\_ (K.Arrow _ _ k) -> k) (kindOf t) ts
+                                         
+pattern AppQuant :: Span -> T.Polarity -> [(Variable, K.Kind)] -> KindedType -> KindedType
+pattern AppQuant s p aks t <- T.AppQuant s _ _ _ p aks t
+  where AppQuant s p aks t = T.AppQuant s (K.Proper s m K.Top) quant abs p aks t
+          where k@(K.Proper _ m _) = kindOf t
+                quant = K.Arrow s abs (K.Proper s m K.Top)
+                abs = foldr (K.Arrow s . snd ) k aks
 
--- pattern AppQuant :: Span -> T.Polarity -> [(Variable, K.Kind)] -> KindedType -> KindedType
--- pattern AppQuant s p aks t <- T.AppQuant s _ _ _ p aks t
---   where AppQuant s p aks t = T.AppQuant s void void void p aks t
+pattern AppForall :: Span -> [(Variable, K.Kind)] -> KindedType -> KindedType
+pattern AppForall s aks t <- T.AppForall s _ _ _ aks t
+  where AppForall s aks t =  AppQuant s T.In aks t
 
--- pattern AppForall :: Span -> [(Variable, K.Kind)] -> KindedType -> KindedType
--- pattern AppForall s aks t <- T.AppForall s _ _ _ aks t
---   where AppForall s aks t =  T.AppForall s void void void aks t
+pattern AppExists :: Span -> [(Variable, K.Kind)] -> KindedType -> KindedType
+pattern AppExists s aks t <- T.AppExists s _ _ _ aks t
+  where AppExists s aks t  = AppQuant s T.Out aks t
 
--- pattern AppExists :: Span -> [(Variable, K.Kind)] -> KindedType -> KindedType
--- pattern AppExists s aks t <- T.AppExists s _ _ _ aks t
---   where AppExists s aks t  = T.AppExists s void void void aks t
+pattern AppArrow :: Span -> K.Multiplicity -> KindedType -> KindedType -> KindedType
+pattern AppArrow s m t u <- T.AppArrow s _ _ m t u
+  where AppArrow s m t u  = T.AppArrow s (K.Proper s m K.Top) arrow m t u
+          -- kind of t -> u -> 1T?
+          where arrow = K.Arrow s (K.lt s) (K.Arrow s (K.lt s) (K.lt s)) 
 
--- pattern AppArrow :: Span -> K.Multiplicity -> KindedType -> KindedType -> KindedType
--- pattern AppArrow s m t u <- T.AppArrow s _ _ m t u
---   where AppArrow s m t u  = T.AppArrow s void void m t u
+pattern AppMessage :: Span -> K.Multiplicity -> T.Polarity -> KindedType -> KindedType
+pattern AppMessage s m p t <- T.AppMessage s _ _ m p t
+  where AppMessage s m p t  = T.AppMessage s msg (K.Arrow s (K.lt s) msg) m p t
+          where msg = K.Proper s m K.Session
+     
+pattern AppTypeMsg :: Span -> T.Polarity -> Variable -> K.Kind -> KindedType -> KindedType
+pattern AppTypeMsg s p a k t <- T.AppTypeMsg s _ _ _ p a k t
+  where AppTypeMsg s p a k t  = T.AppTypeMsg s k' quant abs p a k t
+          where k'@(K.Proper _ m pk) = kindOf t
+                quant = K.Arrow s abs k
+                abs = K.Arrow s k k'
 
--- pattern AppMessage :: Span -> K.Multiplicity -> T.Polarity -> KindedType -> KindedType
--- pattern AppMessage s m p t <- T.AppMessage s _ _ m p t
---   where AppMessage s m p t  = T.AppMessage s void void m p t
+pattern AppLinChoice :: Span -> T.Polarity -> [(Identifier, KindedType)] -> KindedType
+pattern AppLinChoice s p lts <- T.AppLinChoice s _ _ p lts
+  where AppLinChoice s p lts  = T.AppLinChoice s (K.Proper s K.Lin pk) app p lts
+          where pk = foldr (\(_, kindOf -> K.Proper _ _ pk) -> K.join pk) K.Session lts
+                app = foldr (const $ K.Arrow s (K.ls s)) (K.Proper s K.Lin pk) lts
 
--- pattern AppTypeMsg :: Span -> T.Polarity -> Variable -> K.Kind -> KindedType -> KindedType
--- pattern AppTypeMsg s p a k t <- T.AppTypeMsg s _ _ _ p a k t
---   where AppTypeMsg s p a k t  = T.AppTypeMsg s void void void p a k t
+pattern UnChoice :: Span -> T.Polarity -> [Identifier] -> KindedType
+pattern UnChoice s p ls <- T.UnChoice s _ p ls
+  where UnChoice s p ls  = T.UnChoice s (K.uc s) p ls
 
--- pattern AppLinChoice :: Span -> T.Polarity -> [(Identifier, KindedType)] -> KindedType
--- pattern AppLinChoice s p lts <- T.AppLinChoice s _ _ p lts
---   where AppLinChoice s p lts  = T.AppLinChoice s void void p lts
+pattern AppSemi :: Span -> KindedType -> KindedType -> KindedType
+pattern AppSemi s t u <- T.AppSemi s _ _ t u
+  where AppSemi s t u  = T.AppSemi s app semi t u
+          where app = K.Proper s (if pk1 == K.Channel then m1 else K.join m1 m2) (K.meet pk1 pk2) 
+                (K.Proper _ m1 pk1) = kindOf t
+                (K.Proper _ m2 pk2) = kindOf u
+                semi = K.Arrow s (K.ls s) (K.Arrow s (K.ls s) app)
+            
+pattern AppDual :: Span -> KindedType -> KindedType
+pattern AppDual s t <- T.AppDual s _ _ t
+  where AppDual s t  = T.AppDual s (K.Arrow s k k) k t
+          where k = kindOf t
 
--- pattern UnChoice :: Span -> T.Polarity -> [Identifier] -> KindedType
--- pattern UnChoice s p ls <- T.UnChoice s _ p ls
---   where UnChoice s p ls  = T.UnChoice s void p ls
+pattern AppTName :: Span -> Identifier -> [KindedType] -> KindedType
+pattern AppTName s i ts <- T.AppTName s _ _ i ts
+--  where AppTName s i ts  = T.AppTName s void void i ts
 
--- pattern AppSemi :: Span -> KindedType -> KindedType -> KindedType
--- pattern AppSemi s t u <- T.AppSemi s _ _ t u
---   where AppSemi s t u  = T.AppSemi s void void t u
+pattern AppDName :: Span -> K.Kind -> Identifier -> [KindedType] -> KindedType
+pattern AppDName s k i ts <- T.AppDName s k _ i ts
+  where AppDName s k i ts  = T.AppDName s k k' i ts
+          where k' = foldr (\_ (K.Arrow _ _ k) -> k) k ts
+          
+pattern AppVar :: Span -> Variable -> [KindedType] -> KindedType
+pattern AppVar s a ts <- T.AppVar s _ _ a ts
+--  where AppVar s a ts  = T.AppVar s void void a ts
 
--- pattern AppDual :: Span -> KindedType -> KindedType
--- pattern AppDual s t <- T.AppDual s _ _ t
---   where AppDual s t  = T.AppDual s void void t
-
--- pattern AppTName :: Span -> Identifier -> [KindedType] -> KindedType
--- pattern AppTName s i ts <- T.AppTName s _ _ i ts
---   where AppTName s i ts  = T.AppTName s void void i ts
-
--- pattern AppDName :: Span -> Identifier -> [KindedType] -> KindedType
--- pattern AppDName s i ts <- T.AppDName s _ _ i ts
---   where AppDName s i ts  = T.AppDName s void void i ts
-
--- pattern AppVar :: Span -> Variable -> [KindedType] -> KindedType
--- pattern AppVar s a ts <- T.AppVar s _ _ a ts
---   where AppVar s a ts  = T.AppVar s void void a ts
-
--- pattern Tuple :: Span -> [KindedType] -> KindedType
--- pattern Tuple s ts <- T.Tuple s _ _ ts 
---   where Tuple s ts = T.Tuple s void void ts
+pattern Tuple :: Span -> [KindedType] -> KindedType
+pattern Tuple s ts <- T.Tuple s _ _ ts 
+  where Tuple s ts = T.Tuple s (K.Proper s m K.Top) app ts
+          where m = foldr (\(kindOf -> K.Proper _ m _) -> K.meet m ) K.Un ts
+                app = foldr (const $ K.Arrow s (K.lt s)) (K.Proper s m K.Top) ts
   
--- pattern List :: Span -> KindedType -> KindedType
--- pattern List s t <- T.List s _ _ t
---   where List s t  = T.List s void void t
+pattern List :: Span -> KindedType -> KindedType
+pattern List s t <- T.List s _ _ t
+  where List s t  = AppDName s (K.Arrow s (K.lt s) (K.Proper s m K.Top)) (mkListId s) [t]
+          where (K.Proper _ m _) = kindOf t 
+
+kindOf :: KindedType -> K.Kind
+kindOf _ = undefined
