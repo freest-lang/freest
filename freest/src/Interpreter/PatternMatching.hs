f9ce5af033092fb0e69f111db70feb59fdfbe29f
Pedro Ângelo <pjfangelo@hotmail.com>
2026-02-08 21:50:08 +0000

    reorganize Interpreter module, added Values and PatternMatching to cleanly separate functions (it was becomming hard to find functions)

---
+++ freest/src/Interpreter/PatternMatching.hs
@@ -0,0 +1,147 @@
+{- |
+Module      :  Interpreter.PatternMatching
+Copyright   :  © The FreeST Team
+Maintainer  :  freest-lang@listas.ciencias.ulisboa.pt
+
+This module implements FreeST's pattern matching handling.
+-}
+module Interpreter.PatternMatching 
+  (
+    compileFunctionToClosure,
+    resolvePatternMatching
+  ) where
+
+import Control.Monad (zipWithM)
+import Data.Function (on)
+import Data.List (groupBy)
+import qualified Data.Set as Set
+
+import Interpreter.Values (Value(VInt, VFloat, VChar, VCons, VClosure), Binding)
+import qualified Syntax.Expression as E
+import qualified Syntax.Base as B
+
+-- | A clause in a function definition
+type Clause = ([E.Pat], E.RHS)
+
+-- | Match patterns to values, returning a list of associations between variables and values on a success, or a list of the patterns that failed otherwise
+resolvePatternMatching :: E.Pat -> Value -> Either (E.Pat, Value) [Binding]
+resolvePatternMatching (E.IntPat s i) val =
+  case val of
+    VInt i' -> if i == i' then Right [] else Left (E.IntPat s i, val)
+    otherVal -> Left (E.IntPat s i, otherVal)
+resolvePatternMatching (E.FloatPat s f) val =
+  case val of
+    VFloat f' -> if f == f' then Right [] else Left (E.FloatPat s f, val)
+    otherVal -> Left (E.FloatPat s f, otherVal)
+resolvePatternMatching (E.CharPat s c) val =
+  case val of
+    VChar c' -> if c == c' then Right [] else Left (E.CharPat s c, val)
+    otherVal -> Left (E.CharPat s c, otherVal)
+resolvePatternMatching (E.WildPat _ _) _ = Right []
+resolvePatternMatching (E.VarPat _ var) val = Right [(B.external var, val)]
+resolvePatternMatching (E.PackPat _ vars pat) val = undefined
+resolvePatternMatching (E.DConsPat s iden pats) val = do
+  let (B.Identifier s' patIden) = iden
+  case val of
+    VCons iden' vals' -> do
+      -- if data constructors match
+      if patIden == iden' then do
+        -- TODO: check if arity of data constructors matches number of patters/arguments
+        -- get results from pattern matching underlying patterns and terms
+        let binding = zipWithM resolvePatternMatching pats vals'
+        case binding of
+          Left _ -> Left (E.DConsPat s iden pats, val)
+          Right bindings -> Right $ concat bindings
+      else Left (E.DConsPat s iden pats, val)
+    {- VChan (c1, c2) -> do
+      if patIden == "(,)" then do
+        -- get results from pattern matching underlying patterns and terms
+        let binding = zipWithM resolvePatternMatching pats [VChan (c1, c2)]
+        case binding of
+          Left _ -> Left (E.DConsPat s iden pats, val)
+          Right bindings -> Right $ concat bindings
+      else Left (E.DConsPat s iden pats, val) -}
+    otherVal -> Left (E.DConsPat s iden pats, val)
+resolvePatternMatching (E.ChoicePat _ iden pat) val = undefined
+resolvePatternMatching (E.AsPat s var pat) val = do
+  let binding = resolvePatternMatching pat val
+  case binding of
+    Left _ -> Left (E.AsPat s var pat, val)
+    Right bindings -> Right $ (B.external var, val) : bindings
+
+-- | Checks if two patterns are compatible, if in the context of a function, both could be matched against the expression
+-- TODO lacking remaining patterns
+compatibleByPM :: E.Pat -> E.Pat -> Bool
+compatibleByPM E.WildPat{} _ = True
+compatibleByPM _ E.WildPat{} = True
+compatibleByPM E.VarPat{} _ = True
+compatibleByPM _ E.VarPat{} = True
+compatibleByPM (E.DConsPat _ id1 pats1) (E.DConsPat _ id2 pats2) = id1 == id2 && and (zipWith compatibleByPM pats1 pats2)
+compatibleByPM (E.AsPat _ var1 pat1) (E.AsPat _ var2 pat2) = compatibleByPM pat1 pat2
+compatibleByPM p1 p2 = p1 == p2
+
+-- | Compile function definitions with pattern matching into a closure with case-expressions
+compileFunctionToClosure :: [Clause] -> Value
+compileFunctionToClosure = compileFunctionToClosureNaive
+
+compileFunctionToClosureNaive ::  [Clause] -> Value
+compileFunctionToClosureNaive clauses = do
+  let arity = length $ fst $ head clauses
+      -- generate list of new variables, to be made "fresh"
+      newVars = [B.Variable B.nullSpan ("carg" ++ show i) 0 | i <- [1..arity]]
+      -- TODO collect free variables from clauses
+      freeVars = []
+      -- TODO generate fresh variables (taking into account free variables inside clauses as well as generated new ones)
+      freshVars = [B.freshVar newVar (Set.fromList freshVars) | newVar <- newVars]
+      -- generate case expressions
+      cases = clausesToCases freshVars clauses
+      -- attach closures
+  VClosure [E.VarPat B.nullSpan freshVar | freshVar <- freshVars] cases []
+  where
+    clausesToCases :: [B.Variable] -> [Clause] -> E.Exp
+    clausesToCases (freshVar:freshVars) clauses =
+      E.Case B.nullSpan target alternatives
+      where 
+        target = E.Tuple B.nullSpan (map (E.Var B.nullSpan) (freshVar:freshVars))
+        alternatives = map (\(lhs, rhs) -> (E.TuplePat B.nullSpan lhs, rhs)) clauses
+
+
+-- | Convert functions into a closure with cases
+functionToClosure :: [Clause] -> Value
+functionToClosure clauses = do
+  let arity = length $ fst $ head clauses
+      -- generate list of new variables, to be made "fresh"
+      newVars = [B.Variable B.nullSpan ("carg" ++ show i) 0 | i <- [1..arity]]
+      -- TODO collect free variables from clauses
+      freeVars = []
+      -- TODO generate fresh variables (taking into account free variables inside clauses as well as generated new ones)
+      freshVars = [B.freshVar newVar (Set.fromList freshVars) | newVar <- newVars]
+      -- generate case expressions
+      cases = clausesToCases freshVars clauses
+      -- attach closures
+  VClosure [E.VarPat B.nullSpan freshVar | freshVar <- freshVars] cases []
+  where
+    -- group clauses by the leading parameter pattern
+    groupClausesByPatterns :: [Clause] -> [(E.Pat, [Clause])]
+    groupClausesByPatterns clauses =
+      -- extract leading pattern
+      let leadingPat = map (\(pats, rhs) -> (head pats, (tail pats, rhs))) clauses
+      -- group by leading pattern
+          groups = groupBy (compatibleByPM `on` fst) leadingPat
+      -- place clauses that share a leading pattern in the same group
+      -- (TODO: handle compatible patterns)
+      in map (\x -> (fst $ head x, map snd x)) groups
+    -- convert clauses to cases
+    clausesToCases :: [B.Variable] -> [Clause] -> E.Exp
+    clausesToCases (freshVar:freshVars) clauses = 
+      E.Case B.nullSpan (E.Var B.nullSpan freshVar) $ map (\(pat, clauses) -> (pat, convertToRHS freshVars clauses)) groupedClauses
+      where
+        groupedClauses = groupClausesByPatterns clauses
+        -- control recursion, stopping when there's no more patterns to extract
+        convertToRHS :: [B.Variable] -> [Clause] -> E.RHS
+        convertToRHS freshVars clauses =
+          -- if no more patterns, just return first clause RHS
+          -- Warning: if length clauses >= 1, this means there's redundant patterns
+          if null $ fst $ head clauses then snd $ head clauses
+          -- otherwise, recursively call clausesToCases and wrap resulting case in a unguarged rhs
+          else E.UnguardedRHS (clausesToCases freshVars clauses) Nothing
---
+++ freest/src/Interpreter/Values.hs
@@ -0,0 +1,218 @@
+{- |
+Module      :  Interpreter.Values
+Copyright   :  © The FreeST Team
+Maintainer  :  freest-lang@listas.ciencias.ulisboa.pt
+
+This module implements FreeST's values.
+-}
+module Interpreter.Values 
+  (
+    Binding,
+    Env,
+    Value(..),
+    showTups,
+    chan,
+    receive,
+    receiveLabel,
+    send,
+    wait,
+    close,
+    builtins,
+    hsToFstBool,
+    fstToHsBool
+  ) where
+
+import qualified Control.Concurrent.Chan as C (Chan, newChan, readChan, writeChan)
+import Data.Char (chr, ord)
+import Data.Functor (($>), (<&>))
+import GHC.Float
+import System.IO (Handle, hPutStr, stderr, openFile, IOMode (..), hGetChar, hGetLine, hIsEOF, hClose)
+
+import qualified Syntax.Expression as E
+
+-- | A binding of variable to value
+type Binding = (String, Value)
+
+-- | An environment, where bindings from variables to values are stored
+type Env = [Binding]
+
+data Value
+  = VInt Int
+  | VFloat Double
+  | VUnit
+  | VChar Char
+  | VString String
+  | VCons String [Value]
+  | VClosure [E.Pat] E.Exp Env
+  | VBuiltin (Value -> Value)
+  | VIO (IO Value)
+  | VHandle Handle
+  | VLabel String
+  | VFork
+  | VChan ChannelEnd
+  | VSelect String
+
+instance Show Value where
+  show VUnit = "()"
+  show (VInt n) = show n
+  show (VFloat n) = show n
+  show (VChar c) = show c
+  show (VString str) = show str
+  show (VCons str vals) = str ++ " " ++ unwords (map show vals)
+  show (VClosure {}) = "<closure>"
+  show (VBuiltin _) = "<builtin>"
+  show (VIO io) = "<IO>"
+  show (VHandle _) = "<handle>"
+  show (VLabel str) = "<label> string"
+  show (VChan _) = "<chan>"
+  show (VSelect _) = "<select>"
+
+showTups :: [Value] -> String
+showTups [val] = show val
+showTups (val:vals) = show val ++ ", " ++ showTups vals
+
+type ChannelEnd = (C.Chan Value, C.Chan Value)
+
+chan :: IO (ChannelEnd, ChannelEnd)
+chan = do
+  c1 <- C.newChan
+  c2 <- C.newChan
+  return ((c1, c2), (c2, c1))
+
+receive :: ChannelEnd -> IO (Value, ChannelEnd)
+receive c = do
+  v <- C.readChan (fst c)
+  return (v, c)
+
+receiveLabel :: ChannelEnd -> IO String
+receiveLabel c = do
+  VLabel val <- C.readChan (fst c)
+  return val
+
+send :: Value -> ChannelEnd -> IO ChannelEnd
+send v c = do
+  C.writeChan (snd c) v
+  return c
+
+wait :: Value -> Value
+wait (VChan c) =
+  VIO $ C.readChan (fst c)
+
+close :: Value -> IO Value
+close (VChan c) = do
+  C.writeChan (snd c) VUnit
+  return VUnit
+
+builtins :: [(String, Value)]
+builtins = [
+  ("receive", VBuiltin (\(VChan c) -> VIO $ receive c >>= \(val, c) -> return $ VCons "(,)" [val, VChan c])),
+  ("send", VBuiltin (\val -> VBuiltin (\(VChan c) -> VIO $ VChan <$> send val c))),
+  ("wait", VBuiltin wait),
+  ("close", VBuiltin (VIO . close)),
+
+  ("(+)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (x + y)))),
+  ("(-)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (x - y)))),
+  ("subtract", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (x - y)))),
+  ("(*)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (x * y)))),
+  ("(/)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (div x y)))),
+  ("(^)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (x ^ y)))),
+  ("abs", VBuiltin (\(VInt x) -> VInt (abs x))),
+  ("mod", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (mod x y)))),
+  ("rem", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (rem x y)))),
+  ("negate", VBuiltin (\(VInt x) -> VInt (-x))),
+  ("max", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (max x y)))),
+  ("min", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (min x y)))),
+  ("succ", VBuiltin (\(VInt x) -> VInt (succ x))),
+  ("pred", VBuiltin (\(VInt x) -> VInt (pred x))),
+  ("quot", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (quot x y)))),
+  ("even", VBuiltin (\(VInt x) -> hsToFstBool (even x))),
+  ("odd", VBuiltin (\(VInt x) -> hsToFstBool (odd x))),
+  ("gcd", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (gcd x y)))),
+  ("lcm", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> VInt (lcm x y)))),
+
+  ("(+.)", VBuiltin (\(VFloat x) -> VBuiltin (\(VFloat y) -> VFloat (x + y)))),
+  ("(-.)", VBuiltin (\(VFloat x) -> VBuiltin (\(VFloat y) -> VFloat (x - y)))),
+  ("(*.)", VBuiltin (\(VFloat x) -> VBuiltin (\(VFloat y) -> VFloat (x * y)))),
+  ("(/.)", VBuiltin (\(VFloat x) -> VBuiltin (\(VFloat y) -> VFloat (x / y)))),
+  ("negateF", VBuiltin (\(VFloat x) -> VFloat (negate x))),
+  ("maxF", VBuiltin (\(VFloat x) -> VBuiltin (\(VFloat y) -> VFloat (max x y)))),
+  ("minF", VBuiltin (\(VFloat x) -> VBuiltin (\(VFloat y) -> VFloat (min x y)))),
+  ("truncate", VBuiltin (\(VFloat x) -> VInt (truncate x))),
+  ("round", VBuiltin (\(VFloat x) -> VInt (round x))),
+  ("ceiling", VBuiltin (\(VFloat x) -> VInt (ceiling x))),
+  ("floor", VBuiltin (\(VFloat x) -> VInt (floor x))),
+  ("recip", VBuiltin (\(VFloat x) -> VFloat (recip x))),
+  ("pi", VFloat pi),
+  ("exp", VBuiltin (\(VFloat x) -> VFloat (exp x))),
+  ("log", VBuiltin (\(VFloat x) -> VFloat (log x))),
+  ("sqrt", VBuiltin (\(VFloat x) -> VFloat (sqrt x))),
+  ("(**)", VBuiltin (\(VFloat x) -> VBuiltin (\(VFloat y) -> VFloat (x ** y)))),
+  ("logBase", VBuiltin (\(VFloat x) -> VBuiltin (\(VFloat y) -> VFloat (logBase x y)))),
+  ("sin", VBuiltin (\(VFloat x) -> VFloat (sin x))),
+  ("cos", VBuiltin (\(VFloat x) -> VFloat (cos x))),
+  ("tan", VBuiltin (\(VFloat x) -> VFloat (tan x))),
+  ("asin", VBuiltin (\(VFloat x) -> VFloat (asin x))),
+  ("acos", VBuiltin (\(VFloat x) -> VFloat (acos x))),
+  ("atan", VBuiltin (\(VFloat x) -> VFloat (atan x))),
+  ("sinh", VBuiltin (\(VFloat x) -> VFloat (sinh x))),
+  ("cosh", VBuiltin (\(VFloat x) -> VFloat (cosh x))),
+  ("tanh", VBuiltin (\(VFloat x) -> VFloat (tanh x))),
+  ("expm1", VBuiltin (\(VFloat x) -> VFloat (expm1 x))),
+  ("log1p", VBuiltin (\(VFloat x) -> VFloat (log1p x))),
+  ("log1pexp", VBuiltin (\(VFloat x) -> VFloat (log1pexp x))),
+  ("log1mexp", VBuiltin (\(VFloat x) -> VFloat (log1mexp x))),
+  ("fromInteger", VBuiltin (\(VInt x) -> VFloat (fromInteger (toInteger x)))),
+
+  ("(&&)", VBuiltin (\x -> VBuiltin (\y -> hsToFstBool (fstToHsBool x && fstToHsBool y)))),
+  ("(||)", VBuiltin (\x -> VBuiltin (\y -> hsToFstBool (fstToHsBool x || fstToHsBool y)))),
+
+  ("(==)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> hsToFstBool (x == y)))),
+  ("(/=)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> hsToFstBool (x /= y)))),
+  ("(>)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> hsToFstBool (x > y)))),
+  ("(>=)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> hsToFstBool (x >= y)))),
+  ("(<)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> hsToFstBool (x < y)))),
+  ("(<=)", VBuiltin (\(VInt x) -> VBuiltin (\(VInt y) -> hsToFstBool (x <= y)))),
+  ("(>.)", VBuiltin (\(VFloat x) -> VBuiltin (\(VFloat y) -> hsToFstBool (x > y)))),
+  ("(>=.)", VBuiltin (\(VFloat x) -> VBuiltin (\(VFloat y) -> hsToFstBool (x >= y)))),
+  ("(<.)", VBuiltin (\(VFloat x) -> VBuiltin (\(VFloat y) -> hsToFstBool (x < y)))),
+  ("(<=.)", VBuiltin (\(VFloat x) -> VBuiltin (\(VFloat y) -> hsToFstBool (x <= y)))),
+
+  ("chr", VBuiltin (\(VInt x) -> VChar (chr x))),
+  ("ord", VBuiltin (\(VChar c) -> VInt (ord c))),
+
+  ("(^^)", VBuiltin (\(VString str1) -> VBuiltin (\(VString str2) -> VString (str1++str2)))),
+
+  ("show", VBuiltin (VString . show)),
+  ("readBool", VBuiltin (\(VString str) -> hsToFstBool (read str))),
+  ("readInt", VBuiltin (\(VString x) -> VInt (read x))),
+  ("readInt", VBuiltin (\(VString c) -> VChar (read c))),
+
+  -- Parser/Lexer does not accept variables starting with __
+  ("putStrOut", VBuiltin (\val -> VIO $ putStr (show val) $> VUnit)),
+  ("putStrErr", VBuiltin (\val -> VIO $ hPutStr stderr (show val) $> VUnit)),
+  ("getChar", VIO $ getChar <&> VChar),
+  ("getLine", VIO $ getLine <&> VString),
+  ("getContents", VIO $ getContents <&> VString),
+
+  ("openFile", VBuiltin (\(VString path) -> VBuiltin (\(VCons mode []) -> VIO $ case mode of
+    "ReadMode" -> openFile path ReadMode <&> VCons "FileHandle" . (:[]) . VHandle
+    "WriteMode" -> openFile path WriteMode <&> VCons "FileHandle" . (:[]) . VHandle
+    "AppendMode" -> openFile path AppendMode <&> VCons "FileHandle" . (:[]) . VHandle
+    _ -> undefined))),
+  ("putFileStr", VBuiltin (\(VCons "FileHandle" [VHandle handle]) -> VBuiltin (\(VString str) -> VIO $ hPutStr handle str $> VUnit))),
+  ("readFileChar", VBuiltin (\(VCons "FileHandle" [VHandle handle]) -> VIO $ hGetChar handle <&> VChar)),
+  ("readFileLine", VBuiltin (\(VCons "FileHandle" [VHandle handle]) -> VIO $ hGetLine handle <&> VString)),
+  ("isEOF", VBuiltin (\(VCons "FileHandle" [VHandle handle]) -> VIO $ hIsEOF handle <&> hsToFstBool)),
+  ("closeFile", VBuiltin (\(VCons "FileHandle" [VHandle handle]) -> VIO $ hClose handle $> VUnit)),
+
+  ("id", VBuiltin id)]
+
+-- | Convert Haskell's True and False into FreeST's value representation
+hsToFstBool :: Bool -> Value
+hsToFstBool True = VCons "True" []
+hsToFstBool False = VCons "False" []
+
+-- | Extract True and False from FreeST's value representation 
+fstToHsBool :: Value -> Bool
+fstToHsBool (VCons "True" []) = True
+fstToHsBool (VCons "False" []) = False
